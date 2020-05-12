e02dc7eb36096595e60e4c6ead08202e094ad232
Umair Jibran <umairjibran7@gmail.com>
2020-05-13 00:00:20 +0500

    added textanimation

---
+++ OneMonth/Vanilla Javascript/Week 2/createElements.js
@@ -0,0 +1,10 @@
+function buildElements() {
+    for (var i = 0; i < 15; i += 1) {
+        setTimeout(() => {
+
+            var imageBox = document.createElement('div');
+            imageBox.className = 'imageBox';
+            document.getElementById('outerBox').appendChild(imageBox);
+        }, 250 * i);
+    }
+}
