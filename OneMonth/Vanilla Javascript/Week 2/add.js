7a384ec85b83373a58dd4aaa998b52f6d9af911e
Umair Jibran <umairjibran7@gmail.com>
2020-05-12 21:38:28 +0500

    up till lesson 8

---
+++ OneMonth/Vanilla Javascript/Week 2/add.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+    return a + b;
+}
+
+var var1 = parseInt(prompt('Enter Variable 1'));
+var var2 = parseInt(prompt('Enter Variable 2'));
+
+document.write(sum(var1, var2));
---
+++ OneMonth/Vanilla Javascript/Week 2/lenght.js
@@ -0,0 +1,8 @@
+function countLetters(message) {
+    // var length = 0;
+    // for (var i = 0; i < message.length; i++) length += 1;
+    // return length;
+    return message.length;
+}
+
+document.write(countLetters('hello how are you?'));
