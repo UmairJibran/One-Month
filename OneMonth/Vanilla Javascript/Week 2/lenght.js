6f9b6686dc478c7d6f21ca4e3600f86beddbd931
Umair Jibran <umairjibran7@gmail.com>
2020-05-12 22:06:18 +0500

    updated script sequences

--- OneMonth/Vanilla Javascript/Week 2/lenght.js
+++ OneMonth/Vanilla Javascript/Week 2/lenght.js
@@ -2,7 +2,8 @@ function countLetters(message) {
     // var length = 0;
     // for (var i = 0; i < message.length; i++) length += 1;
     // return length;
+    document.writeln(message);
     return message.length;
 }
 
-document.write(countLetters('hello how are you?'));
+// document.write(countLetters('hello how are you?'));
